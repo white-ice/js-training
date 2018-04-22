7168102a04db933d46b95e1ea27afd6e278058e0
Valentin Tonkopey <tonkopey@evergreens.in.ua>
2018-04-22 21:25:32 +0300

    Перехват ошибок, 'try..catch'

--- SomeOtherFeatures/js/lesson.js
+++ SomeOtherFeatures/js/lesson.js
@@ -41,3 +41,58 @@ sayHi("Вася");
 sayHi(["Саша", "Петя", ["Маша", "Юля"]]);
 
 eval('console.log(1+1)');
+
+
+/* Перехват ошибок, "try..catch" */
+
+try {
+    console.log('Начало блока try');
+    lalala
+    console.log('Конец блока try');
+} catch (e) {
+    console.log('Ошибка ' + e.name + ":" + e.message + "\n" + e.stack);
+}
+console.log('Потом код продолжить выполнение...');
+
+
+var data = "Has Error"; // в данных ошибка
+
+try {
+
+    var user = JSON.parse(data); // <-- ошибка при выполнении
+    console.log( user.name ); // не сработает
+
+} catch (e) {
+    // ...выполнится catch
+    console.log( "Извините, в данных ошибка, мы попробуем получить их ещё раз" );
+    console.log( e.name );
+    console.log( e.message );
+}
+
+
+var data = '{ "age": 30 }'; // данные неполны
+
+try {
+
+    var user = JSON.parse(data); // <-- выполнится без ошибок
+
+    if (!user.name) {
+        throw new SyntaxError("Данные некорректны");
+    }
+
+    console.log( user.name );
+
+} catch (e) {
+    console.log( "Извините, в данных ошибка" );
+}
+
+/* Секция finally */
+
+try {
+    console.log( 'try' );
+    if (confirm('Сгенерировать ошибку?')) BAD_CODE();
+} catch (e) {
+    console.log( 'catch' );
+} finally {
+    console.log( 'finally' );
+}
